682db3f0804e9fbf80a2c3c9f52d74398bc32437
Prakhar Srivastava <prakharatdit@gmail.com>
2021-05-19 19:04:13 +0530

    Update TrackierCordovaUtil.java

--- src/android/TrackierCordovaUtil.java
+++ src/android/TrackierCordovaUtil.java
@@ -6,18 +6,22 @@ import org.json.JSONObject;
 
 public class TrackierCordovaUtil {
 
-    public static String getStringVal(String key, JSONObject jsonObject){
+    public static String getStringVal(String key, JSONObject jsonObject) {
         try {
             return jsonObject.getString(key);
-        } catch (JSONException e) {}
+        } catch (JSONException e) {
+            
+        }
         
         return "";
     }
 
-    public static Double getDoubleVal(String key, JSONObject jsonObject){
+    public static Double getDoubleVal(String key, JSONObject jsonObject) {
         try {
             return jsonObject.getDouble(key);
-        } catch (JSONException e) {}
+        } catch (JSONException e) {
+
+        }
         
         return 0.0;
     }
