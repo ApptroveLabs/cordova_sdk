7ab35bcddd841d439ab25e039e20abf0c4b4d0c8
Satyam Jha <ks309599@gmail.com>
2025-08-01 14:59:15 +0530

    Fix remove sdk keys

--- DemoApp/src/app/tab2/tab2.page.ts
+++ DemoApp/src/app/tab2/tab2.page.ts
@@ -15,7 +15,7 @@ export class Tab2Page {
     /* While Initializing the Sdk, You need to pass the three parameter in the TrackierSDKConfig.
      * In first argument, you need to pass the Trackier SDK api key
      * In second argument, you need to pass the environment which can be either "development", "production" or "testing". */
-    var key = "ee9f21fb-5848-4ed9-8d9c-e4093e6d220c"; // Please pass your Development key here
+    var key = "simulator key"; // Please pass your Development key here
     var trackierConfig = new TrackierConfig(key, TrackierEnvironment.Development);
     this.trackierCordovaPlugin.initializeSDK(trackierConfig);
   }
