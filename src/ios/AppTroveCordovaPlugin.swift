e1b07ba545d75fc8c12d997e4028c1ebf37dd8f3
Satyam Jha <satyam@trackier.com>
2026-09-21 14:31:51 +0530

    Update android version

--- src/ios/AppTroveCordovaPlugin.swift
+++ src/ios/AppTroveCordovaPlugin.swift
@@ -24,7 +24,7 @@ class AppTroveCordovaPlugin : CDVPlugin, DeepLinkListener {
             let config = AppTroveSDKConfig(appToken: appToken , env: environment)
             config.setAppSecret(secretId: secretId, secretKey: secretKey)
             config.setSDKType(sdkType: "cordova_sdk")
-            config.setSDKVersion(sdkVersion: "2.0.7")
+            config.setSDKVersion(sdkVersion: "2.0.8")
             config.setDeeplinkListerner(listener: self)
 
             if let regionStr = dict?["region"] as? String {
